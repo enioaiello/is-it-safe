5d99178ce4543d4be82245c8379f502b9a6cd901
ash Bingo <bingoash36@gmail.com>
2025-06-22 17:00:01 +0200

    Revert "ajout des photos de profils, debut du systeme de fame"
    This reverts commit 1ebbfff347cd1d030d8ee48874f27bdeda017bf2.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,6 @@ class DatabaseSeeder extends Seeder
             ForumsSeeder::class,
             CommentsSeeder::class,
             Status_reportsSeeder::class,
-            User_pictureSeeder::class,
         ]);
     }
 }
